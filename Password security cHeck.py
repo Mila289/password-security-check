13a1be86fef107d57af1e61103f09a929a881e93
Mila289 <mila.krasulina@gmail.com>
2025-01-02 19:18:49 +0700

    Обработка булевым типом (False, True).

--- Password security cHeck.py
+++ Password security cHeck.py
@@ -55,3 +55,30 @@ def check_password_strength():
    print(f'{special_count} special characters')
    print(f'Password Score: {strength / 5}')
    print(f'Remarks: {remarks}')
+
+
+def check_pwd(another_pw=False):
+   valid = False
+   if another_pw:
+       choice = input(
+           'Do you want to check another password\'s strength (y/n) : ')
+   else:
+       choice = input(
+           'Do you want to check your password\'s strength (y/n) : ')
+
+   while not valid:
+       if choice.lower() == 'y':
+           return True
+       elif choice.lower() == 'n':
+           print('Exiting...')
+           return False
+       else:
+           print('Invalid input...please try again. \n')
+
+
+if __name__ == '__main__':
+   print('===== Welcome to Password Strength Checker =====')
+   check_pw = check_pwd()
+   while check_pw:
+       check_password_strength()
+       check_pw = check_pwd(True)
